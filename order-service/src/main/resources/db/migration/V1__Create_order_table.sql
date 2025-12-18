c829d68ba717929109e68e52cde68117e62a5615
Leo Huang <HuangLiang50@huawei.com>
2025-12-18 18:31:25 +0800

    add env variable

--- order-service/src/main/resources/db/migration/V1__Create_order_table.sql
+++ order-service/src/main/resources/db/migration/V1__Create_order_table.sql
@@ -1,19 +1,9 @@
 CREATE TABLE `poc_order_db`.`orders`  (
   `oid` int(0) NOT NULL AUTO_INCREMENT,
   `uid` int(0) NOT NULL,
-  `name` nvarchar(100) NULL,
-  `create_time` bigint(0) NULL,
+  `name` varchar(100) NULL,
+  `create_time` bigint NULL,
   `total_price` decimal(10, 2) NULL,
   `state` varchar(20),
   PRIMARY KEY (`oid`)
-);
-
-CREATE TABLE `poc_order_db`.`order_items`  (
-  `id` int(0) NOT NULL AUTO_INCREMENT,
-  `oid` int(0) NOT NULL,
-  `pid` int(0) NOT NULL,
-  `name` nvarchar(100) NULL,
-  `price` decimal(10, 2) NULL,
-  `amount` int(0),
-  PRIMARY KEY (`id`)
 );
